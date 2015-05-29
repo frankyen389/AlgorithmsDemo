181fc2bf5f581936d2c3834f5dfcdb25d462fdb4
frank_yen <javamidlet@gmail.com>
2015-05-29 17:22:57 +0800

    modify MergeSort.java

--- src/my/sort/MergeSort.java
+++ src/my/sort/MergeSort.java
@@ -10,15 +10,51 @@ public class MergeSort {
 			if ((len % 2) != 0) {
 				left = (len / 2) + 1;
 				right = len - left;
+			} else {
+				left = len / 2;
+				right = len - left;
 			}
+			
+			int leftArray [] = new int[left];
+			int rightArray [] = new int[right];
+			for (int i=0; i<left; i++) {
+				leftArray[i] = arrayNum[i];
+			}
+			
+			for (int j=0; j<right; j++) {
+				rightArray[j] = arrayNum[left];
+			}
+			
+			mergeSort(leftArray);
+			
+			mergeSort(rightArray);
+			
+			merge(leftArray, rightArray);
+			
+		} else {
+			return arrayNum;
 		}
 		
 		return arrayNum;
+		
+	}
+	
+	public static int[] merge (int leftArray[], int rightArray[]) {
+		int len = leftArray.length + rightArray.length;
+		int temp[] = new int[len];
+		for (int i=0; i<rightArray.length; i++) {
+			for (int j=0; j<leftArray.length; j++) {
+				
+			}
+		}
+		
+		
+		return null;
 	}
 	
 	public static void main(String[] args) {
 		
-		int arrayNum[] = {38, 27, 43, 3, 9, 82, 10};
+		int arrayNum[] = {38, 27, 43};
 		mergeSort(arrayNum);
 
 	}
